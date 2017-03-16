dfbda3699a60a31a9739789313dd523da36aa633
Michael Fladischer <michael@fladi.at>
2017-03-16 14:35:23 +0100

    Adapt base migration to include new TYPO3 sources.
    This adds:
     * language
     * calendar
     * event

--- src/outpost/base/migrations/0001_initial.py
+++ src/outpost/base/migrations/0001_initial.py
@@ -60,6 +60,20 @@ class Migration(migrations.Migration):
         );
         """.format(settings.MULTICORN.get('campusonline')),
         """
+        CREATE FOREIGN TABLE "typo3"."language" (
+            uid integer,
+            tstamp integer,
+            hidden integer,
+            title varchar,
+            flag varchar,
+            language_isocode varchar
+        )
+        SERVER sqlalchemy OPTIONS (
+            tablename 'sys_language',
+            db_url '{}'
+        );
+        """.format(settings.MULTICORN.get('typo3')),
+        """
         CREATE FOREIGN TABLE "typo3"."category" (
             uid integer,
             tstamp integer,
@@ -80,6 +94,62 @@ class Migration(migrations.Migration):
         )
         SERVER sqlalchemy OPTIONS (
             tablename 'sys_category',
+            db_url '{}'
+        );
+        """.format(settings.MULTICORN.get('typo3')),
+        """
+        CREATE FOREIGN TABLE "typo3"."calendar" (
+            uid integer,
+            tstamp integer,
+            crdate integer,
+            deleted integer,
+            hidden integer,
+            starttime integer,
+            endtime integer,
+            title text,
+            type integer,
+            ext_url varchar,
+            sys_language_uid integer
+        )
+        SERVER sqlalchemy OPTIONS (
+            tablename 'tx_cal_calendar',
+            db_url '{}'
+        );
+        """.format(settings.MULTICORN.get('typo3')),
+        """
+        CREATE FOREIGN TABLE "typo3"."event" (
+            uid integer,
+            tstamp integer,
+            crdate integer,
+            deleted integer,
+            hidden integer,
+            starttime integer,
+            endtime integer,
+            start_date varchar,
+            end_date varchar,
+            start_time integer,
+            end_time integer,
+            allday integer,
+            timezone varchar,
+            title text,
+            calendar_id integer,
+            category_id integer,
+            organizer varchar,
+            location text,
+            teaser text,
+            description text,
+            sys_language_uid integer,
+            tx_mugcal_register integer,
+            tx_mugcal_registration_end integer,
+            tx_mugcal_attendingfees integer,
+            tx_mugcal_www varchar,
+            tx_mugcal_dfppoints integer,
+            tx_mugcal_contact varchar,
+            tx_mugcal_contact_email varchar,
+            tx_mugcal_target integer
+        )
+        SERVER sqlalchemy OPTIONS (
+            tablename 'tx_cal_event',
             db_url '{}'
         );
         """.format(settings.MULTICORN.get('typo3')),
@@ -127,17 +197,18 @@ class Migration(migrations.Migration):
         );
         """.format(settings.MULTICORN.get('typo3')),
         """
+        CREATE VIEW "public"."typo3_language" AS SELECT
+            uid AS id,
+            title,
+            flag,
+            language_isocode AS isocode
+        FROM "typo3"."language"
+        WHERE
+            hidden = 0;
+        """,
+        """
         CREATE VIEW "public"."typo3_category" AS SELECT
             uid AS id,
-            CASE
-                crdate
-            WHEN
-                0
-            THEN
-                NULL
-            ELSE
-                to_timestamp(crdate)
-            END AS created,
             sys_language_uid AS language_id,
             CASE
                 starttime
@@ -160,7 +231,6 @@ class Migration(migrations.Migration):
             title,
             description,
             parent,
-            calendar_id,
             tx_odsosm_marker AS marker
         FROM "typo3"."category"
         WHERE
@@ -168,17 +238,80 @@ class Migration(migrations.Migration):
             hidden = 0;
         """,
         """
+        CREATE VIEW "public"."typo3_calendar" AS SELECT
+            uid AS id,
+            title,
+            sys_language_uid AS language_id
+        FROM "typo3"."calendar"
+        WHERE
+            type = 0 AND
+            deleted = 0 AND
+            hidden = 0;
+        """,
+        """
+        CREATE VIEW "public"."typo3_event" AS SELECT
+            uid AS id,
+            to_date(start_date, 'YYYYMMDD') + CASE
+                allday
+            WHEN
+                1
+            THEN
+                interval '0 hours'
+            ELSE
+                start_time * interval '1 seconds'
+            END AS "start",
+            to_date(end_date, 'YYYYMMDD') + CASE
+                allday
+            WHEN
+                1
+            THEN
+                interval '24 hours'
+            ELSE
+                end_time * interval '1 seconds'
+            END AS "end",
+            allday::boolean AS allday,
+            title,
+            calendar_id,
+            category_id,
+            organizer,
+            location,
+            teaser,
+            description,
+            sys_language_uid AS language_id,
+            tx_mugcal_register::boolean AS register,
+            CASE WHEN
+                tx_mugcal_registration_end > 0
+            THEN
+                to_timestamp(tx_mugcal_registration_end)
+            ELSE
+                NULL
+            END AS registration_end,
+            tx_mugcal_attendingfees::boolean AS attending_fees,
+            tx_mugcal_www AS url,
+            tx_mugcal_dfppoints AS dfp_points,
+            tx_mugcal_contact AS contact,
+            tx_mugcal_contact_email AS email,
+            tx_mugcal_target AS target
+        FROM "typo3"."event"
+        WHERE
+            start_date != '0' AND
+            end_date != '0' AND
+            (starttime = 0 OR to_timestamp(starttime) > NOW()) AND
+            to_date(end_date, 'YYYYMMDD') + CASE
+                allday
+            WHEN
+                1
+            THEN
+                interval '24 hours'
+            ELSE
+                end_time * interval '1 seconds'
+            END > NOW() AND
+            deleted = 0 AND
+            hidden = 0;
+        """,
+        """
         CREATE VIEW "public"."typo3_news" AS SELECT
             uid AS id,
-            CASE
-                crdate
-            WHEN
-                0
-            THEN
-                NULL
-            ELSE
-                to_timestamp(crdate)
-            END AS created,
             sys_language_uid AS language_id,
             CASE
                 starttime
@@ -209,6 +342,8 @@ class Migration(migrations.Migration):
             description
         FROM "typo3"."news"
         WHERE
+            (starttime = 0 OR to_timestamp(starttime) < NOW()) AND
+            (endtime = 0 OR to_timestamp(endtime) > NOW()) AND
             deleted = 0 AND
             hidden = 0 AND
             archive = 0;
@@ -259,7 +394,16 @@ class Migration(migrations.Migration):
         DROP VIEW IF EXISTS "public"."typo3_news";
         """,
         """
+        DROP VIEW IF EXISTS "public"."typo3_event";
+        """,
+        """
+        DROP VIEW IF EXISTS "public"."typo3_calendar";
+        """,
+        """
         DROP VIEW IF EXISTS "public"."typo3_category";
+        """,
+        """
+        DROP VIEW IF EXISTS "public"."typo3_language";
         """,
         """
         DROP FOREIGN TABLE IF EXISTS "campusonline"."gebaeude";
@@ -271,7 +415,16 @@ class Migration(migrations.Migration):
         DROP FOREIGN TABLE IF EXISTS "campusonline"."raum";
         """,
         """
+        DROP FOREIGN TABLE IF EXISTS "typo3"."language";
+        """,
+        """
         DROP FOREIGN TABLE IF EXISTS "typo3"."category";
+        """,
+        """
+        DROP FOREIGN TABLE IF EXISTS "typo3"."event";
+        """,
+        """
+        DROP FOREIGN TABLE IF EXISTS "typo3"."calendar";
         """,
         """
         DROP FOREIGN TABLE IF EXISTS "typo3"."news";
