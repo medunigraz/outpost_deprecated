607bb2aae2912054d5ec17858f4d867b0a2b03e2
Michael Fladischer <michael@fladi.at>
2017-04-18 20:19:38 +0200

    Overengineered upload filename factory.

--- src/outpost/base/utils.py
+++ src/outpost/base/utils.py
@@ -3,13 +3,10 @@ from pathlib import PurePosixPath
 from base64 import urlsafe_b64encode
 
 
-class Uuid4Upload(object):
+class Uuid4Upload(str):
 
-    def __init__(self, instance, filename):
+    def __new__(cls, instance, filename):
         f = PurePosixPath(filename)
         u = urlsafe_b64encode(uuid4().bytes).decode('ascii').rstrip('=')
         p = PurePosixPath(instance.__module__, instance._meta.object_name)
-        self.path = p.joinpath(u).with_suffix(f.suffix).as_posix()
-
-    def __unicode__(self):
-        return self.path
+        return str.__new__(cls, p.joinpath(u).with_suffix(f.suffix))
