8b7a332317fb01aa4e45f24a8cd8bbfaebde6972
Michael Fladischer <michael@fladi.at>
2017-04-26 11:05:10 +0200

    Improve geo admin views for polymorphic models.

--- src/outpost/geo/admin.py
+++ src/outpost/geo/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
+from polymorphic.admin import (
+    PolymorphicChildModelAdmin,
+    PolymorphicChildModelFilter,
+    PolymorphicParentModelAdmin,
+)
 from reversion.admin import VersionAdmin
 
 from . import models
+
+
+@admin.register(models.Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'campusonline', 'origin')
+
+
+@admin.register(models.Floor)
+class FloorAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'level', 'campusonline', 'origin')
+    list_filter = (
+        'level',
+        'building',
+    )
 
 
 class SourceEdgeInline(admin.TabularInline):
@@ -15,14 +34,40 @@ class DestinationEdgeInline(admin.TabularInline):
     fk_name = "destination"
 
 
+class NodeChildAdmin(PolymorphicChildModelAdmin):
+    base_model = models.Node
+
+
+@admin.register(models.Node)
+class NodeParentAdmin(PolymorphicParentModelAdmin):
+    base_model = models.Node
+    child_models = (models.Door, models.Room)
+    list_filter = (
+        PolymorphicChildModelFilter,
+        'level',
+    )
+
+
+@admin.register(models.Door)
+class DoorAdmin(NodeChildAdmin):
+    base_model = models.Door
+    list_display = ('__str__', 'level', 'origin')
+    list_filter = (
+        'level',
+    )
+
+
 @admin.register(models.Room)
-class RoomAdmin(VersionAdmin, admin.ModelAdmin):
+class RoomAdmin(NodeChildAdmin):
+    base_model = models.Room
     list_display = ('__str__', 'level', 'campusonline', 'origin')
     inlines = [
-        # DoorInline,
         DestinationEdgeInline,
         SourceEdgeInline,
     ]
+    list_filter = (
+        'level',
+    )
 
 
 @admin.register(models.Background)
@@ -37,26 +82,6 @@ class LevelAdmin(OrderedModelAdmin):
 
 @admin.register(models.Edge)
 class EdgeAdmin(VersionAdmin, admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Node)
-class NodeAdmin(VersionAdmin, admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Building)
-class BuildingAdmin(VersionAdmin, admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Floor)
-class FloorAdmin(VersionAdmin, admin.ModelAdmin):
-    list_display = ('__str__', 'level', 'campusonline', 'origin')
-
-
-@admin.register(models.Beacon)
-class BeaconAdmin(admin.ModelAdmin):
     pass
 
 
