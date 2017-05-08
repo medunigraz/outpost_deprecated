60b2a56688ce3fa79f7632f4695f034deb8cb46a
Michael Fladischer <michael@fladi.at>
2017-05-08 11:32:11 +0200

    Add celery beat task to periodically update materialized views.

---
+++ src/outpost/base/tasks.py
@@ -0,0 +1,23 @@
+from celery.task import PeriodicTask
+from datetime import timedelta
+from django.db import connection
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class RefreshViewsTask(PeriodicTask):
+    run_every = timedelta(hours=1)
+    query = """
+    SELECT oid::regclass::text FROM pg_class WHERE relkind = 'm';
+    """
+
+    def run(self, **kwargs):
+        logger.debug('Refreshing materialized views.')
+        with connection.cursor() as relations:
+            relations.execute(self.query)
+            for rel in relations:
+                logger.debug('Refresh materialized view: %s', rel)
+                with connection.cursor() as refresh:
+                    refresh.execute('REFRESH MATERIALIZED VIEW {}'.format(rel[0]))
