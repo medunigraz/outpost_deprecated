472d1dfe80aa2580a80c417e5f6bb8781fe3a98b
Michael Fladischer <michael@fladi.at>
2017-08-04 20:04:44 +0200

    Allow to select models for autocomplete response.

--- src/outpost/api/views.py
+++ src/outpost/api/views.py
@@ -13,6 +13,16 @@ class AutocompleteViewSet(HaystackViewSet):
 
         .../?q=<Word>
 
+    To limit results to certain models use the `m` parameter:
+
+        .../?q=<Word>&m=<Model>,<Model>,...
+
+    Possible models are currently:
+
+     * `geo.Room`
+     * `structure.Organization`
+     * `structure.Person`
+
     The `ctype` property determines the content type of each suggested item.
     This can be used to do further queries at other endpoints.
     """
@@ -28,3 +38,16 @@ class AutocompleteViewSet(HaystackViewSet):
     filter_backends = (
         HaystackAutocompleteFilter,
     )
+
+    def get_queryset(self, index_models=[]):
+        queryset = self.object_class()._clone()
+        if 'm' in self.request.GET:
+            allowed = self.request.GET.get('m').split(',')
+            models = filter(
+                lambda m: m._meta.label in allowed,
+                self.index_models
+            )
+            queryset = queryset.models(*models)
+        else:
+            queryset = queryset.models(*self.index_models)
+        return queryset
