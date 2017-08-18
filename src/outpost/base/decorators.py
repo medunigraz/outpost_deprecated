e4cd8b1d25ed584b924df008588891b39b8162cf
Michael Fladischer <michael@fladi.at>
2017-08-18 15:30:04 +0200

    Fix pre_delete signal handler registration.

--- src/outpost/base/decorators.py
+++ src/outpost/base/decorators.py
@@ -3,6 +3,7 @@ from functools import wraps
 from django.db.models.signals import (
     post_delete,
     post_save,
+    pre_delete,
     pre_save,
 )
 
@@ -22,14 +23,17 @@ def signal_connect(cls):
         signal.connect(wrapper, sender=cls)
         return wrapper
 
-    if hasattr(cls, 'pre_save'):
-        cls.pre_save = connect(pre_save, cls.pre_save)
+    if hasattr(cls, 'post_delete'):
+        cls.post_delete = connect(post_delete, cls.post_delete)
 
     if hasattr(cls, 'post_save'):
         cls.post_save = connect(post_save, cls.post_save)
 
-    if hasattr(cls, 'post_delete'):
-        cls.post_delete = connect(post_save, cls.post_delete)
+    if hasattr(cls, 'pre_delete'):
+        cls.pre_delete = connect(pre_delete, cls.pre_delete)
+
+    if hasattr(cls, 'pre_save'):
+        cls.pre_save = connect(pre_save, cls.pre_save)
 
     return cls
 
