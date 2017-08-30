14203f3af42fc230568c394c8ea7aff91091c079
Michael Fladischer <michael@fladi.at>
2017-08-30 18:38:23 +0200

    Initialize sys.argv. before loading celery.

--- src/outpost/celery.py
+++ src/outpost/celery.py
@@ -1,13 +1,14 @@
 import os
 import sys
+
+if not hasattr(sys, 'argv'):
+    setattr(sys, 'argv', [''])
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outpost.settings')
 
 from django.conf import settings
-
-if not hasattr(sys, 'argv'):
-    setattr(sys, 'argv', [''])
 
 app = Celery('outpost')
 app.config_from_object('django.conf:settings')
