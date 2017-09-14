7b87932eaf12bb6c186f4c857e687be6c599be08
Michael Fladischer <michael@fladi.at>
2017-09-14 22:05:14 +0200

    Clean up materialized view refresh task.

--- src/outpost/base/tasks.py
+++ src/outpost/base/tasks.py
@@ -2,26 +2,27 @@ import logging
 from datetime import timedelta
 
 from celery.task import PeriodicTask
-from django.db import connection
 from celery_haystack.tasks import CeleryHaystackUpdateIndex
 
 logger = logging.getLogger(__name__)
 
 
-class RefreshViewsTask(PeriodicTask):
+class RefreshMaterializedViewsTask(PeriodicTask):
     run_every = timedelta(hours=1)
-    query = """
+    query = '''
     SELECT oid::regclass::text FROM pg_class WHERE relkind = 'm';
-    """
+    '''
 
     def run(self, **kwargs):
+        from django.db import connection
         logger.debug('Refreshing materialized views.')
         with connection.cursor() as relations:
             relations.execute(self.query)
-            for rel in relations:
+            for (rel,) in relations:
                 logger.debug('Refresh materialized view: %s', rel)
                 with connection.cursor() as refresh:
-                    refresh.execute('REFRESH MATERIALIZED VIEW {}'.format(rel[0]))
+                    refresh.execute('REFRESH MATERIALIZED VIEW {}'.format(rel))
+        connection.close()
 
 
 class UpdateHaystackTask(PeriodicTask):
