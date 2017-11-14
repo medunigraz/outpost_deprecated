7531cdf78e75750d1a0ef77b70dd51fc7ef159b7
Michael Fladischer <michael@fladi.at>
2017-11-14 11:37:34 +0100

    Change notification object ID field to CharField to fit UUIDs.

--- src/outpost/base/models.py
+++ src/outpost/base/models.py
@@ -88,7 +88,7 @@ class Notification(models.Model):
         ContentType,
         on_delete=models.CASCADE
     )
-    object_id = models.PositiveIntegerField()
+    object_id = models.CharField(max_length=36)
     content_object = GenericForeignKey(
         'content_type',
         'object_id'
