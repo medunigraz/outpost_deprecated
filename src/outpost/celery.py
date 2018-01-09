9fa1ff410a16e2a317b83f3dcc3559d28f07bb6f
Michael Fladischer <michael@fladi.at>
2018-01-09 11:52:57 +0100

    Set celery config namespace and configure separate task route for video tasks.

--- src/outpost/celery.py
+++ src/outpost/celery.py
@@ -11,5 +11,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outpost.settings')
 from django.conf import settings
 
 app = Celery('outpost')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
