ced5222ae845b1ad6bfffc7fd917de11c7ad212b
Michael Fladischer <michael@fladi.at>
2018-10-03 12:39:14 +0200

    [Video] Remove frontend views as they are handled by Crates.

--- src/outpost/video/urls.py
+++ src/outpost/video/urls.py
@@ -11,105 +11,37 @@ from . import (
 nginx = [
     url(
         r'^publish/$',
-        views.PublishView.as_view(),
+        views.PublishRTMPView.as_view(),
         name='publish'
     ),
     url(
         r'^done/$',
-        views.PublishDoneView.as_view(),
+        views.PublishDoneRTMPView.as_view(),
         name='done'
     ),
 ]
-recording = [
-    url(
-        r'^$',
-        views.RecordingListView.as_view(),
-        name='list'
-    ),
-    url(
-        r'^(?P<pk>\d+)$',
-        views.RecordingDetailView.as_view(),
-        name='detail'
-    ),
-    url(
-        r'^(?P<pk>\d+)/(?P<exporter>\w+)/$',
-        views.RecordingExportView.as_view(),
-        name='export'
-    ),
-]
-recorder = [
-    url(
-        r'^$',
-        views.RecorderListView.as_view(),
-        name='list'
-    ),
-    url(
-        r'^(?P<pk>\d+)$',
-        views.RecorderDetailView.as_view(),
-        name='detail'
-    ),
-    url(
-        r'^(?P<pk>\d+)/epiphan/channel/(?P<channel>\d+)/$',
-        views.EpiphanChannelView.as_view(),
-        name='epiphan-channel'
-    ),
-]
-event = [
-    url(
-        r'^$',
-        views.EventListView.as_view(),
-        name='list'
-    ),
-    url(
-        r'^create/$',
-        views.EventCreateView.as_view(),
-        name='create'
-    ),
+dash = [
     url(
         r'^(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/$',
-        views.EventDetailView.as_view(),
-        name='detail'
-    ),
-    url(
-        r'^(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/media/(?:(?P<media>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/)?$',
-        views.EventMediaView.as_view(),
-        name='media'
-    ),
-]
-publish = [
-    url(
-        r'^dash/(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/$',
         views.DASHView.as_view(),
-        name='dash'
+        name='index'
     ),
     url(
-        r'^dash/video/(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/(?P<path>.+)$',
+        r'^video/(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/(?P<path>.+)$',
         views.DASHMediaView.as_view(model=models.DASHVideoVariant),
-        name='dash-video'
+        name='video'
     ),
     url(
-        r'^dash/audio/(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/(?P<path>.+)$',
+        r'^audio/(?P<pk>[0-9a-f]{8}\-[0-9a-f]{4}\-4[0-9a-f]{3}\-[89ab][0-9a-f]{3}\-[0-9a-f]{12})/(?P<path>.+)$',
         views.DASHMediaView.as_view(model=models.DASHAudio),
-        name='dash-audio'
+        name='audio'
     ),
 ]
 
 urlpatterns = [
     url(
-        r'^event/',
-        include(event, namespace='event')
-    ),
-    url(
-        r'^publish/',
-        include(publish, namespace='publish')
-    ),
-    url(
-        r'^recording/',
-        include(recording, namespace='recording')
-    ),
-    url(
-        r'^recorder/',
-        include(recorder, namespace='recorder')
+        r'^dash/',
+        include(dash, namespace='dash')
     ),
     url(
         r'^nginx/',
--- src/outpost/video/views.py
+++ src/outpost/video/views.py
@@ -62,7 +62,7 @@ class NginxRTMPBackend(View):
         return self.handle(request, stream)
 
 
-class PublishView(NginxRTMPBackend):
+class PublishRTMPView(NginxRTMPBackend):
 
     def handle(self, request, stream):
         stream.active = Broadcast.objects.create(stream=stream)
@@ -70,7 +70,7 @@ class PublishView(NginxRTMPBackend):
         return HttpResponse()
 
 
-class PublishDoneView(NginxRTMPBackend):
+class PublishDoneRTMPView(NginxRTMPBackend):
 
     def handle(self, request, stream):
         stream.active.end = timezone.now()
@@ -78,154 +78,6 @@ class PublishDoneView(NginxRTMPBackend):
         stream.active = None
         stream.save()
         return HttpResponse()
-
-
-class RecordingListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
-    model = Recording
-    permission_required = 'video.change_recording'
-    paginate_by = 10
-
-
-class RecordingDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
-    model = Recording
-    permission_required = 'video.change_recording'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        classes = Export.__subclasses__()
-        exporters = [(c._meta.verbose_name, c.__name__) for c in classes]
-        context['exporters'] = sorted(exporters, key=lambda x: x[1])
-        return context
-
-
-class RecordingExportView(LoginRequiredMixin, PermissionRequiredMixin, View):
-    permission_required = 'video.change_recording'
-
-    def post(self, request, pk, exporter):
-        task = ExportTask.delay(pk, exporter, request.build_absolute_uri('/'))
-        return JsonResponse(
-            {
-                'task': task.id,
-            }
-        )
-
-
-class RecorderListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
-    model = Recorder
-    permission_required = 'video.change_recorder'
-
-
-class RecorderDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
-    model = Recorder
-    permission_required = 'video.change_recorder'
-
-
-class EpiphanChannelView(LoginRequiredMixin, PermissionRequiredMixin, View):
-    permission_required = 'video.change_recorder'
-    http_method_names = [
-        'get',
-        'start',
-        'stop',
-    ]
-
-    def get(self, request, pk, channel):
-        epiphan = get_object_or_404(Epiphan, pk=pk)
-        obj = get_object_or_404(EpiphanChannel, pk=channel, epiphan=epiphan)
-        return JsonResponse(obj.response())
-
-    def start(self, request, pk, channel):
-        epiphan = get_object_or_404(Epiphan, pk=pk)
-        obj = get_object_or_404(EpiphanChannel, pk=channel, epiphan=epiphan)
-        obj.start()
-        return JsonResponse(obj.response())
-
-    def stop(self, request, pk, channel):
-        epiphan = get_object_or_404(Epiphan, pk=pk)
-        obj = get_object_or_404(EpiphanChannel, pk=channel, epiphan=epiphan)
-        obj.stop()
-        return JsonResponse(obj.response())
-
-
-class EventListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
-    model = Event
-    permission_required = 'video.add_event'
-
-
-class EventCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    model = Event
-    permission_required = 'video.add_event'
-    fields = (
-        'title',
-        'subject',
-        'description',
-        'license',
-        'rights',
-        'presenters',
-        'contributors',
-        'room',
-    )
-
-    def get_success_url(self):
-        return reverse('video:event:list')
-
-
-class EventUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
-    model = Event
-    permission_required = 'video.change_event'
-
-
-class EventDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
-    model = Event
-    permission_required = 'video.change_event'
-
-    def delete(self, request, pk):
-        event = get_object_or_404(Event, pk=pk)
-        event.delete()
-        return HttpResponse()
-
-
-class EventMediaView(LoginRequiredMixin, PermissionRequiredMixin, View):
-    permission_required = 'video.change_event'
-    mapping = {
-        'video/mp4': EventVideo,
-        'video/webm': EventVideo,
-        'video/ogg': EventVideo,
-        'audio/mp4': EventAudio,
-        'audio/x-m4a': EventAudio,
-        'audio/mpeg': EventAudio,
-        'audio/webm': EventAudio,
-        'audio/ogg': EventAudio,
-        'audio/wav': EventAudio,
-        'audio/wave': EventAudio,
-        'audio/flac': EventAudio,
-        'audio/aac': EventAudio,
-    }
-
-    def post(self, request, pk, media=None):
-        event = get_object_or_404(Event, pk=pk)
-        f = request.FILES.get('file')
-        if f.content_type not in self.mapping:
-            return HttpResponseBadRequest(
-                'Unsupported mimetype: {}'.format(f.content_type)
-            )
-        obj = self.mapping.get(f.content_type)(event=event, name=f.name)
-        obj.data.save(f.name, f)
-        return JsonResponse(obj.response())
-
-    def get(self, request, pk, media):
-        event = get_object_or_404(Event, pk=pk)
-        obj = get_object_or_404(EventMedia, pk=media)
-        return JsonResponse(obj.response())
-
-    def delete(self, request, pk, media):
-        event = get_object_or_404(Event, pk=pk)
-        obj = get_object_or_404(EventMedia, pk=media)
-        obj.delete()
-        return JsonResponse({})
-
-
-class PublishView(LoginRequiredMixin, DetailView):
-    model = Publish
 
 
 class DASHView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
