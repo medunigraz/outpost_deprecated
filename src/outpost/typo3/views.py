0adaa07eadcc883e0b9f8a220d3925ccedbc7a9a
Michael Fladischer <michael@fladi.at>
2018-10-31 15:40:22 +0100

    [TYPO3] Pass image data through if no modification is required.

--- src/outpost/typo3/views.py
+++ src/outpost/typo3/views.py
@@ -24,17 +24,22 @@ class MediaView(View):
         media = get_object_or_404(models.Media, pk=pk)
         try:
             req = requests.get(media.url)
+            response = HttpResponse()
+            response['Cache-Control'] = 'private,max-age=604800'
+            if not width:
+                response['Content-Type'] = req.headers.get('Content-Type')
+                response.write(req.content)
+                return response
             with Image.open(io.BytesIO(req.content)) as img:
                 fmt = img.format
-                response = HttpResponse()
-                if width:
-                    width = int(width)
-                    if img.width > width:
-                        height = int(img.height * (width / float(img.width)))
-                        img = img.resize((width, height), Image.ANTIALIAS)
-                img.save(response, format=fmt)
                 response['Content-Type'] = Image.MIME[fmt]
-                response['Cache-Control'] = 'private,max-age=604800'
+                width = int(width)
+                if img.width <= width:
+                    response.write(req.content)
+                    return response
+                height = int(img.height * (width / float(img.width)))
+                img = img.resize((width, height), Image.ANTIALIAS)
+                img.save(response, format=fmt, quality=95, optimize=True)
                 return response
         except Exception as e:
             logger.warn(f'Failed to load image blob: {e}')
