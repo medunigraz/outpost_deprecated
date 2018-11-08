fcc4d39707e209bd09395bdfa40142ddf05107fb
Michael Fladischer <michael@fladi.at>
2018-11-08 10:25:36 +0100

    [CAMPUSOnline] Fix indention.

--- src/outpost/campusonline/urls.py
+++ src/outpost/campusonline/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(
-            r'^avatar/(?P<hash>[\w\d]+)$',
-            views.PrivateAvatarView.as_view(),
-            name='avatar-private'
-        ),
+    url(
+        r'^avatar/(?P<hash>[\w\d]+)$',
+        views.PrivateAvatarView.as_view(),
+        name='avatar-private'
+    ),
 ]
