1e656850ef88f89f04173db73797643fa09fc35b
Michael Fladischer <michael@fladi.at>
2019-01-16 15:07:43 +0100

    [gunicorn] Use environment to override worker count.

--- src/outpost/gunicorn.py
+++ src/outpost/gunicorn.py
@@ -1,4 +1,8 @@
 import multiprocessing
+import os
 
-workers = int(multiprocessing.cpu_count() / 2)
+workers = int(os.environ.get(
+    'OUTPOST_GUNICORN_WORKERS',
+    multiprocessing.cpu_count() / 2
+))
 worker_class = 'uvicorn.workers.UvicornWorker'
