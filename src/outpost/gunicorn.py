10d79d0ea73cee994d79d6a02811605c2cdbba02
Michael Fladischer <michael@fladi.at>
2019-03-18 15:07:26 +0100

    Switch to uvicorn.workers.UvicornH11Worker.

--- src/outpost/gunicorn.py
+++ src/outpost/gunicorn.py
@@ -5,4 +5,4 @@ workers = int(os.environ.get(
     'OUTPOST_GUNICORN_WORKERS',
     multiprocessing.cpu_count() / 2
 ))
-worker_class = 'uvicorn.workers.UvicornWorker'
+worker_class = 'uvicorn.workers.UvicornH11Worker'
